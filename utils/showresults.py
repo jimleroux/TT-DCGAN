bc65922c4ac3ce8567d920b9cfe849f3afef12ec
NBerty <39064188+NBerty@users.noreply.github.com>
2019-04-18 15:32:30 -0400

    Reconstruction plot has been improved.

--- utils/showresults.py
+++ utils/showresults.py
@@ -5,16 +5,16 @@ import numpy as np
 import torch
 
 def show_recons(images, num_epoch, num_samp, path):
-    fig, ax = plt.subplots(num_samp, 2, figsize=(num_samp, 2))
+    fig, ax = plt.subplots(num_samp, 2, figsize=(3, 3))
     for i, j in itertools.product(range(num_samp), range(2)):
         ax[i, j].get_xaxis().set_visible(False)
         ax[i, j].get_yaxis().set_visible(False)
 
-    for i in range(2):
-        test_images = images[i]
-        for j in range(num_samp):
+    for j in range(2):
+        test_images = images[j]
+        for i in range(num_samp):
             ax[i, j].cla() 
-            im = np.transpose(test_images[j, :].cpu().data.numpy(), (1,2,0))
+            im = np.transpose(test_images[i].squeeze(0).cpu().data.numpy(), (1,2,0))
             ax[i, j].imshow(im/2+0.5)
 
     label = 'Epoch {0}'.format(num_epoch)
